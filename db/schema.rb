4533975fffabe526d2f2d83cdfb4ed5bd605256b
Pete Kane <petekaneee@icloud.com>
2014-07-27 15:48:04 -0500

    none

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140224203311) do
+ActiveRecord::Schema.define(version: 20140727193957) do
+
+  create_table "student_prefs", force: true do |t|
+    t.integer "user_id"
+    t.date    "start_date"
+    t.date    "completion_date"
+    t.integer "hrs_needed"
+    t.integer "max_travel_distance"
+    t.boolean "does_gender_matter"
+    t.integer "creds_pursuing"
+    t.boolean "want_addiction_cert_super"
+    t.boolean "want_play_cert_super"
+    t.integer "most_important_factors"
+  end
+
+  create_table "supervisor_prefs", force: true do |t|
+    t.integer "license_num"
+    t.integer "years_of_supervision"
+    t.integer "how_manu_students_want"
+    t.integer "most_important_factors"
+    t.integer "clinical_settings"
+    t.integer "how_advertise"
+  end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                             default: "", null: false
+    t.string   "encrypted_password",                default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                     default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -27,6 +49,23 @@ ActiveRecord::Schema.define(version: 20140224203311) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone",                  limit: 12
+    t.text     "change_theory"
+    t.string   "address"
+    t.integer  "zip"
+    t.string   "gender"
+    t.boolean  "admin"
+    t.boolean  "addiction_certified"
+    t.boolean  "play_certified"
+    t.text     "who_for_coffee"
+    t.text     "education"
+    t.boolean  "remote_supervision"
+    t.boolean  "group_supervision"
+    t.integer  "dollars_per_hr"
+    t.string   "how_hear_about_us"
+    t.integer  "prefs_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
