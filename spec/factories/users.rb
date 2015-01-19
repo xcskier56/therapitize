9b7a4911aa7f384abefa61e6bd4a385f8cd42d2d
Doug DeBold <ddebold56@gmail.com>
2015-01-18 18:02:26 -0600

    Fixed user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -30,8 +30,10 @@
 
 FactoryGirl.define do
   factory :user do
-    name "Test User"
-    email "test@example.com"
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name}
+    email { Faker::Internet.email }
     password "please123"
+    password_confirmation "please123"
   end
 end
